d253362f480f563a4cbf04cb4fbe2883981a2850
lufitweb <markkayuto123@gmail.com>
2026-04-05 14:59:55 +0700

    change page navigation defaults to H/J, remove hardcoded keybind hints

--- src/bindings.cpp
+++ src/bindings.cpp
@@ -760,10 +760,10 @@ void BindingsConfiguration::generateDefaults()
 	}
 	if (notBound(k = stringToKey("ctrl-p")))
 		bind(k, Actions::Type::SetSelectedItemsPriority);
-	if (notBound(k = stringToKey("ctrl-f")))
-		bind(k, Actions::Type::NextSearchPage);
-	if (notBound(k = stringToKey("ctrl-b")))
+	if (notBound(k = stringToKey("H")))
 		bind(k, Actions::Type::PrevSearchPage);
+	if (notBound(k = stringToKey("J")))
+		bind(k, Actions::Type::NextSearchPage);
 	if (notBound(k = stringToKey("q")))
 		bind(k, Actions::Type::Quit);
 }
